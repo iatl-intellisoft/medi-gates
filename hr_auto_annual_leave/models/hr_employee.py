b613b54d40c78a0decce69d373f77b4a2edfadb6
rajaamohammed <rajaa.mohammed@iatl-intellisoft.com>
2025-09-01 15:30:41 +0200

    hr

--- hr_auto_annual_leave/models/hr_employee.py
+++ hr_auto_annual_leave/models/hr_employee.py
@@ -22,7 +22,7 @@ class HrEmployee(models.Model):
     def allocate_annual_leave_one_year(self):
         today = fields.Date.today()
         employees = self.search([
-            ('contract_id.date_start', '!=', False, 'contract_id.state', '==', 'open'),
+            ('contract_id.date_start', '!=', False, 'contract_id.state', '=', 'open'),
         ])
 
         annual_leave_type = self.env['hr.leave.type'].search([
