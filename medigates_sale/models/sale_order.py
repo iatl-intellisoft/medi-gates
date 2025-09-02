37cfca9b9af29a155fedda4a0c5fcef7d61051e0
rajaamohammed <rajaa.mohammed@iatl-intellisoft.com>
2025-09-02 13:15:01 +0200

    sale

--- medigates_sale/models/sale_order.py
+++ medigates_sale/models/sale_order.py
@@ -53,11 +53,10 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    private_custom = fields.Boolean(related='order_id.partner_id.private_custom', string="Private Customer")
+        private_custom = fields.Boolean(string="Private Customer")
 
     @api.onchange('order_id.partner_id.private_custom')
     def _onchange_partner_private_discount_lock(self):
         if self.order_id.partner_id.private_custom:
-            self.discount = 0.0 
-
+            self.private_custom = True
 
