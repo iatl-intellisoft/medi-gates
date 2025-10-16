bd6e1b475fba9c8342e88b6b3e3aeb1f842c5e5f
rajaamohammed <rajaa.mohammed@iatl-intellisoft.com>
2025-10-16 15:24:01 +0200

    return report stock

--- order_return/reports/report_stock_move_incoming.py
+++ order_return/reports/report_stock_move_incoming.py
@@ -1,19 +1,21 @@
-from odoo import models, fields, api, _
+from odoo import models, fields
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    purchase_ref = fields.Char(string='Purchase Ref', compute='_compute_purchase_ref', store=True)
-    currency_id = fields.Many2one('res.currency', string='Currency', compute='_compute_purchase_ref', store=True)
-    stock_out_ref = fields.Char(string='Stock Out Ref', related='picking_id.origin', store=True)
-
-    @api.depends('picking_id')
-    def _compute_purchase_ref(self):
-        for move in self:
-            purchase = self.env['purchase.order'].search([('name', '=', move.picking_id.origin)], limit=1)
-            if purchase:
-                move.purchase_ref = purchase.name
-                move.currency_id = purchase.currency_id
-            else:
-                move.purchase_ref = move.picking_id.origin or ''
-                move.currency_id = False
+    purchase_ref = fields.Char(
+        string='Purchase Ref',
+        related='purchase_line_id.order_id.name',
+        store=True
+    )
+    currency_id = fields.Many2one(
+        'res.currency',
+        string='Currency',
+        related='purchase_line_id.order_id.currency_id',
+        store=True
+    )
+    stock_out_ref = fields.Char(
+        string='Stock Out Ref',
+        related='picking_id.origin',
+        store=True
+    )
