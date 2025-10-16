932f3e0b5dc8608a23576fad68130ebdc16910a7
rajaamohammed <rajaa.mohammed@iatl-intellisoft.com>
2025-10-16 08:16:31 +0200

    return report stock

--- order_return/reports/return_sale_report_by_quantity.py
+++ order_return/reports/return_sale_report_by_quantity.py
@@ -191,8 +191,9 @@ class SaleReturnReportQuanitiy(models.Model):
             LEFT JOIN product_template t ON p.product_tmpl_id = t.id
             LEFT JOIN uom_uom u ON u.id = l.product_uom
             LEFT JOIN uom_uom u2 ON u2.id = t.uom_id
-            LEFT JOIN stock_move sm ON sm.id = sml.move_id
-            LEFT JOIN stock_move_line sml ON sm.stock_return_id = s.id AND sml.product_id = l.product_id AND sm.state = 'done'
+            LEFT JOIN stock_move_line sml ON sml.picking_id = s.sale_picking_id AND sml.product_id = l.product_id AND sml.qty_done > 0
+            LEFT JOIN stock_move sm ON sm.id = sml.move_id AND sm.state = 'done'
+            LEFT JOIN stock_production_lot lot ON sml.lot_id = lot.id
             LEFT JOIN stock_lot lot ON sml.lot_id = lot.id
 
             JOIN {currency_table} ON account_currency_table.company_id = s.company_id
