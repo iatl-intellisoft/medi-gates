150e41f59321195cf0ef16b2c16dcc73e2b51e07
rajaamohammed <rajaa.mohammed@iatl-intellisoft.com>
2025-10-16 14:40:46 +0200

    return report stock

--- order_return/reports/report_stock_move_incoming.py
+++ order_return/reports/report_stock_move_incoming.py
@@ -1,30 +1,21 @@
 from odoo import models, fields
 
-class ReportStockMoveIncoming(models.AbstractModel):
-    _name = 'report.order_return.report_stock_move_incoming'
-    _description = 'Incoming Stock Move Report'
+class StockMoveIncoming(models.Model):
+    _inherit = 'stock.move'
 
-    def _get_report_values(self, docids, data=None):
-        domain = [('picking_type_code', '=', 'incoming')]
-        stock_moves = self.env['stock.move'].search(domain)
-
-        # Prepare data for the template
-        docs = []
-        for move in stock_moves:
-            docs.append({
-                'purchase_ref': move.purchase_line_id.order_id.name if move.purchase_line_id else '',
-                'product_name': move.product_id.display_name,
-                'qty': move.product_uom_qty,
-                'price_unit': move.price_unit,
-                'currency': move.purchase_line_id.order_id.currency_id.name if move.purchase_line_id else '',
-                'stock_out_ref': move.picking_id.origin or '',
-                'location_dest': move.location_dest_id.display_name,
-                'state': move.state,
-            })
-
-        return {
-            'doc_ids': docids,
-            'doc_model': 'stock.move',
-            'docs': docs,
-            'company': self.env.company,
-        }
+    purchase_ref = fields.Char(
+        string='Purchase Ref',
+        related='purchase_line_id.order_id.name',
+        store=True
+    )
+    currency_id = fields.Many2one(
+        'res.currency',
+        string='Currency',
+        related='purchase_line_id.order_id.currency_id',
+        store=True
+    )
+    stock_out_ref = fields.Char(
+        string='Stock Out Ref',
+        related='picking_id.origin',
+        store=True
+    )
