b3eed7c5838961f180f19124a5caf30cea62a73e
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-15 17:33:24 +0200

    Update account_move.py

--- payment_term_delivery_date/models/account_move.py
+++ payment_term_delivery_date/models/account_move.py
@@ -8,7 +8,7 @@ from odoo import api, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def _recompute_delivery_date_due(self):
+    def _apply_delivery_date_due_date(self):
         for move in self:
             if not move.invoice_payment_term_id:
                 continue
@@ -16,21 +16,24 @@ class AccountMove(models.Model):
             if not move.delivery_date_act:
                 continue
 
-            term_line = move.invoice_payment_term_id.line_ids.filtered(
+            delivery_line = move.invoice_payment_term_id.line_ids.filtered(
                 lambda l: l.delay_type == 'delivery_date_act'
             )[:1]
 
-            if not term_line:
+            if not delivery_line:
                 continue
 
             move.invoice_date_due = (
-                move.delivery_date_act +
-                timedelta(days=term_line.nb_days)
+                move.delivery_date_act
+                + timedelta(days=delivery_line.nb_days)
             )
 
-    @api.onchange('invoice_payment_term_id', 'delivery_date_act')
-    def _onchange_delivery_payment_term(self):
-        self._recompute_delivery_date_due()
+    @api.onchange(
+        'delivery_date_act',
+        'invoice_payment_term_id'
+    )
+    def _onchange_delivery_date_due(self):
+        self._apply_delivery_date_due_date()
 
     def write(self, vals):
         res = super().write(vals)
@@ -41,13 +44,6 @@ class AccountMove(models.Model):
         ):
             self.filtered(
                 lambda m: m.state == 'draft'
-            )._recompute_delivery_date_due()
+            )._apply_delivery_date_due_date()
 
-        return res
-        
-    def action_post(self):
-        res = super().action_post()
-    
-        self._recompute_delivery_date_due()
-    
         return res
