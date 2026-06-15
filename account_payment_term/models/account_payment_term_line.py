52709d6cf22990ee33b0260c35e8db0ff9e2c550
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-15 13:21:07 +0200

    Refactor account payment term logic by removing class
    Removed the AccountPaymentTerm class and its _compute_terms method, simplifying the payment term logic.

--- account_payment_term/models/account_payment_term_line.py
+++ account_payment_term/models/account_payment_term_line.py
@@ -1,5 +1,4 @@
 from odoo import fields, models
-from datetime import timedelta
 
 class AccountPaymentTermLine(models.Model):
     _inherit = "account.payment.term.line"
@@ -9,55 +8,3 @@ class AccountPaymentTermLine(models.Model):
             ("actual_delivery", "Days After Actual Delivery Date")
         ]
     )
-class AccountPaymentTerm(models.Model):
-    _inherit = "account.payment.term"
-
-    def _compute_terms(
-        self,
-        date_ref,
-        currency,
-        company,
-        tax_amount,
-        tax_amount_currency,
-        sign,
-        untaxed_amount,
-        untaxed_amount_currency,
-        cash_rounding=None,
-    ):
-        result = super()._compute_terms(
-            date_ref,
-            currency,
-            company,
-            tax_amount,
-            tax_amount_currency,
-            sign,
-            untaxed_amount,
-            untaxed_amount_currency,
-            cash_rounding,
-        )
-
-        move = self.env.context.get("move")
-
-        if not move:
-            return result
-
-        if not move.delivery_date_act:
-            return result
-
-        lines = self.line_ids.sorted("sequence")
-
-        for index, line in enumerate(lines):
-
-            if (
-                line.delay_type == "actual_delivery"
-                and index < len(result)
-            ):
-
-                amount, due_date = result[index]
-
-                result[index] = (
-                    amount,
-                    move.delivery_date_act + timedelta(days=line.nb_days)
-                )
-
-        return result
