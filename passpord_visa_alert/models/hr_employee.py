00ca6abeb36242e334bd3b3aea40343c140c56d1
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-16 12:07:10 +0200

    Update hr_employee.py

--- passpord_visa_alert/models/hr_employee.py
+++ passpord_visa_alert/models/hr_employee.py
@@ -97,74 +97,74 @@ class HrEmployee(models.Model):
 
             employee.visa_alert_sent = True 
 
-    passport_warning = fields.Boolean(
-        compute='_compute_passport_warning'
-    )
+    # passport_warning = fields.Boolean(
+    #     compute='_compute_passport_warning'
+    # )
 
-    passport_warning_message = fields.Char(
-        compute='_compute_passport_warning'
-    )
+    # passport_warning_message = fields.Char(
+    #     compute='_compute_passport_warning'
+    # )
 
-    @api.depends('passport_expiration_date')
-    def _compute_passport_warning(self):
+    # @api.depends('passport_expiration_date')
+    # def _compute_passport_warning(self):
 
-        today = fields.Date.today()
+    #     today = fields.Date.today()
 
-        for rec in self:
+    #     for rec in self:
 
-            rec.passport_warning = False
-            rec.passport_warning_message = False
+    #         rec.passport_warning = False
+    #         rec.passport_warning_message = False
 
-            if not rec.passport_expiration_date:
-                continue
+    #         if not rec.passport_expiration_date:
+    #             continue
 
-            if rec.passport_expiration_date <= today:
-                rec.passport_warning = True
-                rec.passport_warning_message = (
-                    "Passport has expired."
-                )
+    #         if rec.passport_expiration_date <= today:
+    #             rec.passport_warning = True
+    #             rec.passport_warning_message = (
+    #                 "Passport has expired."
+    #             )
 
-            elif rec.passport_expiration_date <= today + timedelta(days=21):
-                rec.passport_warning = True
-                rec.passport_warning_message = (
-                    "Passport will expire within 21 days."
-                )
+    #         elif rec.passport_expiration_date <= today + timedelta(days=21):
+    #             rec.passport_warning = True
+    #             rec.passport_warning_message = (
+    #                 "Passport will expire within 21 days."
+    #             )
 
-    def cron_passport_expiry_alert(self):
+    # def cron_passport_expiry_alert(self):
 
-        today = fields.Date.today()
-        target_date = today + timedelta(days=21)
+    #     today = fields.Date.today()
+    #     target_date = today + timedelta(days=21)
 
-        employees = self.search([
-            ('passport_expiration_date', '=', target_date)
-        ])
+    #     employees = self.search([
+    #         ('passport_expiration_date', '=', target_date)
+    #     ])
 
-        template = self.env.ref(
-            'passpord_visa_alert.email_template_passport_expiry'
-        )
+    #     template = self.env.ref(
+    #         'passpord_visa_alert.email_template_passport_expiry'
+    #     )
 
-        hr_group = self.env.ref(
-            'hr.group_hr_user'
-        )
+    #     hr_group = self.env.ref(
+    #         'hr.group_hr_user'
+    #     )
 
-        hr_users = hr_group.user_ids.filtered(
-            lambda u: u.partner_id.email
-        )
+    #     hr_users = hr_group.user_ids.filtered(
+    #         lambda u: u.partner_id.email
+    #     )
 
-        for employee in employees:
+    #     for employee in employees:
 
-            recipients = hr_users
+    #         recipients = hr_users
 
-            if employee.parent_id \
-                    and employee.parent_id.user_id:
-                recipients |= employee.parent_id.user_id
+    #         if employee.parent_id \
+    #                 and employee.parent_id.user_id:
+    #             recipients |= employee.parent_id.user_id
 
-            for user in recipients:
-                template.send_mail(
-                    employee.id,
-                    email_values={
-                        'email_to':
-                            user.partner_id.email
-                    },
-                    force_send=True
-                )
+    #         for user in recipients:
+    #             template.send_mail(
+    #                 employee.id,
+    #                 email_values={
+    #                     'email_to':
+    #                         user.partner_id.email
+    #                 },
+    #                 force_send=True
+                # )
