29cf7cee99f069521d450d33868f347abe66a4d3
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-06-16 09:20:06 +0200

    Update account_payment_term.py

--- payment_term_delivery_date/models/account_payment_term.py
+++ payment_term_delivery_date/models/account_payment_term.py
@@ -2,16 +2,12 @@
 from odoo import api, models, fields, _
 from odoo.exceptions import ValidationError
 
-
 class AccountPaymentTermLine(models.Model):
-    """Extend Payment Term Lines with a new due-date computation option
-    based on the Actual Delivery Date of the related invoice.
-    """
     _inherit = 'account.payment.term.line'
 
-    delay_type  = fields.Selection(
+    delay_type = fields.Selection(
         selection_add=[
-            ('delivery_date_act', '15 Days After Actual Delivery Date'),
+            ('delivery_date_act', 'Days After Actual Delivery Date'),
         ],
         ondelete={'delivery_date_act': 'set default'},
     )
