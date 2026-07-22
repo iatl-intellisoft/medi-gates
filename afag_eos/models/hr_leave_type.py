52012822f17226f915477fdcd26bb54273dbcfd6
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-07-22 10:09:27 +0200

    Delete afag_eos directory

--- afag_eos/models/hr_leave_type.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class HolidaysType(models.Model):
-    _inherit = "hr.leave.type"
-
-    eos_computed = fields.Boolean('Computed in EOS')
