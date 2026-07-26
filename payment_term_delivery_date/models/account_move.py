54eaa166ffbc9030e490383945223af93c0aff73
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-07-26 07:34:52 -0700

    Update account_move.py

--- payment_term_delivery_date/models/account_move.py
+++ payment_term_delivery_date/models/account_move.py
@@ -16,7 +16,7 @@ class AccountMove(models.Model):
 
         return super()._get_payment_terms_computation_date()
 
-    @api.onchange('delivery_date_act')
-    def _onchange_delivery_date_act(self):
-        self._recompute_payment_terms_lines()
+    # @api.onchange('delivery_date_act')
+    # def _onchange_delivery_date_act(self):
+    #     self._recompute_payment_terms_lines()
 
