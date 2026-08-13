ea46242b40b4bf84682da629547f535efbf0748b
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-08-13 13:45:15 +0200

    Update account_payment_term.py

--- payment_term_delivery_date/models/account_payment_term.py
+++ payment_term_delivery_date/models/account_payment_term.py
@@ -8,9 +8,14 @@ class AccountPaymentTermLine(models.Model):
 
     delay_type = fields.Selection(
         selection_add=[
-            ('delivery_date_act', 'Days After Actual Delivery Date'),
+            (
+                'delivery_date_act',
+                'Days After Actual Delivery Date'
+            ),
         ],
-        ondelete={'delivery_date_act': 'set default'},
+        ondelete={
+            'delivery_date_act': 'set default'
+        },
     )
 
 class AccountPaymentTerm(models.Model):
@@ -42,19 +47,55 @@ class AccountPaymentTerm(models.Model):
     #             value['date'] = delivery_date_act + timedelta(days=line.nb_days)
     #     return result
                             
+    # def _compute_terms(
+    #     self,
+    #     date_ref,
+    #     currency,
+    #     company,
+    #     tax_amount,
+    #     tax_amount_currency,
+    #     sign,
+    #     untaxed_amount,
+    #     untaxed_amount_currency,
+    #     cash_rounding=None,
+    #     delivery_date_act=False,
+    # ):
+    #     result = super()._compute_terms(
+    #         date_ref,
+    #         currency,
+    #         company,
+    #         tax_amount,
+    #         tax_amount_currency,
+    #         sign,
+    #         untaxed_amount,
+    #         untaxed_amount_currency,
+    #         cash_rounding=cash_rounding,
+    #     )
+
+    #     if not delivery_date_act:
+    #         return result
+
+    #     for line, value in zip(self.line_ids, result):
+    #         if line.delay_type == 'delivery_date_act':
+    #             value['date'] = (
+    #                 delivery_date_act +
+    #                 timedelta(days=line.nb_days)
+    #             )
+
+    #     return result
     def _compute_terms(
-        self,
-        date_ref,
-        currency,
-        company,
-        tax_amount,
-        tax_amount_currency,
-        sign,
-        untaxed_amount,
-        untaxed_amount_currency,
-        cash_rounding=None,
-        delivery_date_act=False,
-    ):
+            self,
+            date_ref,
+            currency,
+            company,
+            tax_amount,
+            tax_amount_currency,
+            sign,
+            untaxed_amount,
+            untaxed_amount_currency,
+            cash_rounding=None,
+            delivery_date_act=False,
+        ):
         result = super()._compute_terms(
             date_ref,
             currency,
@@ -73,8 +114,8 @@ class AccountPaymentTerm(models.Model):
         for line, value in zip(self.line_ids, result):
             if line.delay_type == 'delivery_date_act':
                 value['date'] = (
-                    delivery_date_act +
-                    timedelta(days=line.nb_days)
+                    delivery_date_act
+                    + timedelta(days=line.nb_days)
                 )
 
         return result
