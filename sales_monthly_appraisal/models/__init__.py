8857a0b4f1ed94b62d21e19a90c56337a59210b8
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-08-20 08:15:01 +0200

    Add files via upload

---
+++ sales_monthly_appraisal/models/__init__.py
@@ -0,0 +1,5 @@
+from . import sales_appraisal_kpi_category
+from . import sales_appraisal_kpi_definition
+from . import sales_appraisal_target
+from . import sales_appraisal
+from . import sales_appraisal_kpi
