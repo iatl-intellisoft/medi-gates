4056365632e84f0995b2386d4395796909d96899
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-08-20 10:41:34 +0200

    Delete sales_monthly_appraisal directory

--- sales_monthly_appraisal/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import sales_appraisal_kpi_category
-from . import sales_appraisal_kpi_definition
-from . import sales_appraisal_target
-from . import sales_appraisal
-from . import sales_appraisal_kpi
