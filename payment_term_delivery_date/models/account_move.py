146505450ad798660e173e4b11c02ae4e131dc09
montaga-elkhatim <montaga.elkhatim@gmail.com>
2026-08-23 11:15:58 +0200

    Enhance payment terms computation with delivery date

--- payment_term_delivery_date/models/account_move.py
+++ payment_term_delivery_date/models/account_move.py
@@ -4,9 +4,26 @@ from datetime import timedelta
 from odoo import api, models
 from odoo import models
 
-
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
+    def _get_payment_terms_computation_date(self):
+        self.ensure_one()
+        if self.delivery_date_act:
+            return self.delivery_date_act
+        return super()._get_payment_terms_computation_date()
+
+    def _recompute_payment_terms_lines(self):
+        for move in self:
+            super(AccountMove, move.with_context(
+                delivery_date_act=move.delivery_date_act
+            ))._recompute_payment_terms_lines()
+        return True
+
+    @api.onchange('delivery_date_act')
+    def _onchange_delivery_date_act(self):
+        if self.invoice_payment_term_id:
+            self._recompute_payment_terms_lines()
 
     def _get_payment_terms_computation_date(self):
         self.ensure_one()
@@ -16,6 +33,9 @@ class AccountMove(models.Model):
 
         return super()._get_payment_terms_computation_date()
 
-    # @api.onchange('delivery_date_act')
-    # def _onchange_delivery_date_act(self):
-    #     self._recompute_payment_terms_lines()
+    
+    @api.onchange('delivery_date_act')
+    def _onchange_delivery_date_act(self):
+        for move in self:
+            if move.invoice_payment_term_id:
+                move._recompute_payment_terms_lines()
